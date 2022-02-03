7d15e3da8acc6f0b94636023b90b5110df6ad9ad
Aditya <akaditya394@gmail.com>
2022-02-03 23:10:49 +0530

    error state manage done

--- src/components/Users/AddUser/AddUser.jsx
+++ src/components/Users/AddUser/AddUser.jsx
@@ -2,15 +2,24 @@ import React, { useState } from "react";
 import Card from "../../UI/Card/Card.jsx";
 import classes from "./AddUser.module.css";
 import Button from "../../UI/Button/Button";
+import ErrorModal from "../../UI/ErrorModal/ErrorModal";
 
 const AddUser = (props) => {
   const addUserHandler = (event) => {
     event.preventDefault();
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: "Error Occured",
+        message: "Please nter non-empty values",
+      });
       return;
     }
 
     if (+enteredAge < 1) {
+      setError({
+        title: "Error Occured",
+        message: "Please enter valid age (>0)",
+      });
       return;
     }
 
@@ -21,6 +30,7 @@ const AddUser = (props) => {
 
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
+  const [error, setError] = useState();
 
   const usernameChangeHandler = (event) => {
     setEnteredUsername(event.target.value);
@@ -30,30 +40,43 @@ const AddUser = (props) => {
     setEnteredAge(event.target.value);
   };
 
+  const errorHandler = () => {
+      setError(null);
+  };
+
   return (
-    <Card className={classes.input}>
-      <form onSubmit={addUserHandler}>
-        <label forHTML="username">Username</label>
-        <input
-          id="username"
-          type="text"
-          name="username"
-          onChange={usernameChangeHandler}
-          value={enteredUsername}
+    <div>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
         />
-        <label forHTML="age">Age</label>
-        <input
-          id="age"
-          type="number"
-          name="age"
-          onChange={ageChangeHandler}
-          value={enteredAge}
-        />
-        <Button type="submit" onClick={addUserHandler}>
-          Add User
-        </Button>
-      </form>
-    </Card>
+      )}
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label forHTML="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            name="username"
+            onChange={usernameChangeHandler}
+            value={enteredUsername}
+          />
+          <label forHTML="age">Age</label>
+          <input
+            id="age"
+            type="number"
+            name="age"
+            onChange={ageChangeHandler}
+            value={enteredAge}
+          />
+          <Button type="submit" onClick={addUserHandler}>
+            Add User
+          </Button>
+        </form>
+      </Card>
+    </div>
   );
 };
 
--- src/components/Users/UsersList/UsersList.jsx
+++ src/components/Users/UsersList/UsersList.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import Card from "../../UI/Card/Card";
-import ErrorModal from "../../UI/ErrorModal/ErrorModal";
 
 import classes from "./UsersList.module.css";
 
 const UsersList = (props) => {
   return (
     <div>
-    <ErrorModal title="Oops! something went wrong" message="an error occured" />
       <Card className={classes.users}>
         <ul>
           {props.users.map((user) => (
